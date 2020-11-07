3142628160824773ab486d9b1d2b7365184760c7
James Yin <54519648+yin202@users.noreply.github.com>
2020-11-06 22:03:29 -0600

    Update bot.py
    Test some basic commands

--- bot.py
+++ bot.py
@@ -4,11 +4,24 @@ from discord.ext import commands
 # Token from Discord Developer Website
 TOKEN = 'Nzc0NDY1NDUwMzM5MDc0MDc5.X6YLKA.CEd15xcXw7BrMxd0Ij6vX1MOIKE'
 
-clientCom = commands.Bot(command_prefix = "LoLWork")
+client = commands.Bot(command_prefix = "!")
 
-# Determines if bot is active not
-@clientCom.event
+# Determines if bot is active or not
+@client.event
 async def on_ready():
     print('LoL Workout Bot is ready!')
 
-clientCom.run(TOKEN)
+@client.command()
+async def numArgs(client, *arg):
+    await client.send('Passed {} number of arguments.'.format(len(arg)))
+
+@client.command()
+async def echo(client, *message):
+    output = ''
+    for word in message:
+        output += word
+        output += ' '
+    await client.send('Echo: {}'.format(output))
+
+
+client.run(TOKEN)
