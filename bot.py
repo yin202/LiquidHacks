0d2b7f2dcb44a897df59596ed2fc2ee7e6d63869
JHsu02 <73502236+JHsu02@users.noreply.github.com>
2020-11-07 04:41:08 -0500

    v1.0 JOE ED. The based update
    + Addition of first embedded messages!
    + Color is customizable

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ TOKEN = 'Nzc0NDY1NDUwMzM5MDc0MDc5.X6YLKA.CEd15xcXw7BrMxd0Ij6vX1MOIKE'
 client = commands.Bot(command_prefix = "!")
 
 # Determines if bot is active or not
+
+# James's work
 @client.event
 async def on_ready():
     print('LoL Workout Bot is ready!')
@@ -23,5 +25,14 @@ async def echo(client, *message):
         output += ' '
     await client.send('Echo: {}'.format(output))
 
+# Joseph's random stuff
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!based'):
+        embedVar = discord.Embed(title="BASED", description="based_test", color=0x61ff33)
+        embedVar.add_field(name="check_f1", value="Yes", inline=False)
+        embedVar.add_field(name="check_f2", value="Very Based", inline=False)
+        await message.channel.send(embed=embedVar)
 
 client.run(TOKEN)
