7e1f8c08c0014ce3a99d43a1107da30f240318ae
Kevin Tian <ktiiann@gmail.com>
2020-11-07 15:18:53 -0500

    Added methods for ChampLookup, ChampLookupInternal, BuildMatchList

--- bot.py
+++ bot.py
@@ -60,36 +60,88 @@ async def work(client, *message):
         else:
             await client.send("Enter one region and one summoner name.")
         return
-           
 
+    buildMatchList(message[0], message[1])   # Builds match tables for last 7 matches
+    await client.send(buildMatchList(message[0], message[1])[0])    # Sends the first match table to the client
+
+    #champs = df.loc[:, "champion"]
+    #embedVar = discord.Embed(
+    #    title="Champions", description="Player Champions", color=0x61ff33)
+    #for i in champs:
+    #    embedVar.add_field(name=i, value=i, inline=False)
+    #await client.send(embed=embedVar)
+
+
+# Champ Lookup command
+@client.command()
+async def champLookup(client, *message):
     lol_watcher = LolWatcher('RGAPI-d121ef6b-adab-4d13-8831-a10fb9ae71fe')
-    region = message[0]
-    me = lol_watcher.summoner.by_name(region, message[1])
-    ranked_stats = lol_watcher.league.by_summoner(region, me['id'])
-
-    match_list = lol_watcher.match.matchlist_by_account(region, me['accountId'])
-    entr = []
-    for i in range (0,7):
-        entr_row = {}
-        entr_row['platformId'] = match_list['matches'][i]['platformId']
-        entr_row['gameId'] = match_list['matches'][i]['gameId']
-        entr_row['champion'] = match_list['matches'][i]['champion']
-        entr_row['queue'] = match_list['matches'][i]['queue']
-        entr_row['season'] = match_list['matches'][i]['season']
-        entr_row['timestamp'] = match_list['matches'][i]['timestamp']
-        entr_row['role'] = match_list['matches'][i]['role']
-        entr_row['lane'] = match_list['matches'][i]['lane']
-        entr.append(entr_row)
-
-    df = pd.DataFrame(entr)
-    # await client.send(df)
-
-    champs = df.loc[:, "champion"]
+    latest = lol_watcher.data_dragon.versions_for_region('na1')['n']['champion']
+    static_champ_list = lol_watcher.data_dragon.champions(latest, False, 'en_US')
+    champ_dict = {}
+    for key in static_champ_list['data']:
+        row = static_champ_list['data'][key]
+        champ_dict[row['key']] = row['id']
+    champName = champ_dict[message[0]]
+    print(champ_dict)
     embedVar = discord.Embed(
-        title="Champions", description="Player Champions", color=0x61ff33)
-    for i in champs:
-        embedVar.add_field(name=i, value=i, inline=False)
+        title="Champion Lookup", description="Returns the Champion based off of ID", color=0x61ff33)
+    embedVar.add_field(name="Champion Name", value=champName, inline=False)
+    embedVar.add_field(name="Champion ID", value=message, inline=False)
     await client.send(embed=embedVar)
+
+@client.command()
+async def testBML(client, *message):
+    await client.send(buildMatchList('sorairo', 'na1')[1])
+
+def champLookupInternal(id):
+    lol_watcher = LolWatcher('RGAPI-d121ef6b-adab-4d13-8831-a10fb9ae71fe')
+    latest = lol_watcher.data_dragon.versions_for_region('na1')['n']['champion']
+    static_champ_list = lol_watcher.data_dragon.champions(latest, False, 'en_US')
+    champ_dict = {}
+    for key in static_champ_list['data']:
+        row = static_champ_list['data'][key]
+        champ_dict[row['key']] = row['id']
+    champName = champ_dict[str(id)]
+    return champName
+
+def buildMatchList(username, user_region):
+    lol_watcher = LolWatcher('RGAPI-d121ef6b-adab-4d13-8831-a10fb9ae71fe')
+
+    user = lol_watcher.summoner.by_name(user_region, username)
+
+    match_list = lol_watcher.match.matchlist_by_account(user_region, user['accountId'])
+
+    listOfMatches = []
+    listOfMatchesDict = []
+
+    for i in range(0, 7):
+        tempMatch = match_list['matches'][i]
+        match_details = lol_watcher.match.by_id(user_region, tempMatch['gameId'])
+        participants = []
+        for row in match_details['participants']:
+            participants_row = {}
+            participants_row['champion'] = row['championId']
+            participants_row['spell1'] = row['spell1Id']
+            participants_row['spell2'] = row['spell2Id']
+            participants_row['win'] = row['stats']['win']
+            participants_row['kills'] = row['stats']['kills']
+            participants_row['deaths'] = row['stats']['deaths']
+            participants_row['assists'] = row['stats']['assists']
+            participants_row['totalDamageDealt'] = row['stats']['totalDamageDealt']
+            participants_row['goldEarned'] = row['stats']['goldEarned']
+            participants_row['champLevel'] = row['stats']['champLevel']
+            participants_row['totalMinionsKilled'] = row['stats']['totalMinionsKilled']
+            participants_row['item0'] = row['stats']['item0']
+            participants_row['item1'] = row['stats']['item1']
+            participants_row['championName'] = champLookupInternal(row['championId'])
+            participants.append(participants_row)
+        df = pd.DataFrame(participants)
+        listOfMatchesDict.append(participants)
+        listOfMatches.append(df)
+
+    return listOfMatches
+
 
 
 
